c73dc3e48857d669e3b538faf536ab7b22561999
Yuriy Frolov <yfrolou@mail.ru>
2025-10-25 23:24:44 +0300

    Добавляет разметку

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,49 @@
-/* в этот файл добавляет скрипты*/
+// Range Slider
+const rangeSlider = document.querySelector('#range-slider');
+const formatForSlider = {
+  from: function (formattedValue) {
+    return Number(formattedValue);
+  },
+  to: function (numericValue) {
+    return Math.round(numericValue);
+  },
+};
+
+noUiSlider.create(rangeSlider, {
+  start: [200, 800],
+  connect: true,
+  range: {
+    min: 0,
+    max: 1000,
+  },
+  format: formatForSlider,
+});
+
+const formatValues = [
+  document.getElementById('min_num'),
+  document.getElementById('max_num'),
+];
+
+rangeSlider.noUiSlider.on('update', (values, handle) => {
+  formatValues[handle].value = values[handle];
+});
+
+
+// Switch Toggle
+document.querySelectorAll('input[name="country"]').forEach((radio) => {
+  radio.addEventListener('change', function () {
+    document.querySelectorAll('.option').forEach((option) => {
+      option.classList.remove('selected');
+    });
+    if (this.checked) {
+      this.closest('.option').classList.add('selected');
+    }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const checkedRadio = document.querySelector('input[name="country"]:checked');
+  if (checkedRadio) {
+    checkedRadio.closest('.option').classList.add('selected');
+  }
+});
